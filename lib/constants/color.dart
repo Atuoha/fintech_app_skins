0ea90cb1771ebf88bd461797b3fb2007fed911d8
Atuoha <atuohainitiatives@gmail.com>
2022-08-15 09:01:34 +0100

    assets setup

---
+++ lib/constants/color.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+const primaryColor = Color(0XFF083E9E);
+const accentColor = Color(0XFF06446F);
+const cardColor1 = Color(0XFF4770B9);
+const cardColor2 = Color(0XFF471C7E);
+const cardColor3 = Color(0XFFD94E05);
+const addNewCardBg = Color(0XFFCDD8EB);
+const fundWalletBg = Color(0XFFCCE4CD);
+const withdrawBg = Color(0XFFE9CCCC);
+const greyShade1 = Color(0XFF8D8D8D);
+const greyShade2 = Color(0XFFCFCFCF);
