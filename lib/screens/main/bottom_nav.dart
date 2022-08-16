e1b560b8fdca13c0eb5e8e6dbc200e3e4f8dc1b8
Atuoha <atuohainitiatives@gmail.com>
2022-08-16 14:44:04 +0100

    refractured the bottomNav using curved navigation bar

--- lib/screens/main/bottom_nav.dart
+++ lib/screens/main/bottom_nav.dart
@@ -1,14 +1,16 @@
 import 'package:fintech_app_ui/constants/color.dart';
-import 'package:fintech_app_ui/screens/main/history.dart';
-import 'package:fintech_app_ui/screens/main/home.dart';
-import 'package:fintech_app_ui/screens/main/subscriptions.dart';
-import 'package:fintech_app_ui/screens/main/wallet.dart';
+import 'history.dart';
+import 'home.dart';
+import 'subscriptions.dart';
+import 'wallet.dart';
 import 'package:flutter/material.dart';
-
 import 'account.dart';
+import 'package:flutter/services.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class BottomNav extends StatefulWidget {
   static const routeName = '/bottomNav';
+
   const BottomNav({Key? key}) : super(key: key);
 
   @override
@@ -16,6 +18,7 @@ class BottomNav extends StatefulWidget {
 }
 
 class _BottomNavState extends State<BottomNav> {
+  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
   var currentIndex = 0;
   final _screens = const [
     HomeScreen(),
@@ -33,103 +36,171 @@ class _BottomNavState extends State<BottomNav> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
+    );
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-        onTap: changeScreen,
-        selectedItemColor: hoverColor,
-        unselectedItemColor: greyShade2,
-        showUnselectedLabels: true,
-        elevation: 0,
+      bottomNavigationBar: CurvedNavigationBar(
+        key: _bottomNavigationKey,
+        index: currentIndex,
+        height: 60.0,
         items: [
-          BottomNavigationBarItem(
-            icon: Container(
-              padding: const EdgeInsets.only(top: 10),
-              decoration: BoxDecoration(
-                border: currentIndex == 0
-                    ?  Border(
-                        top: BorderSide(
-                          color: hoverColor,
-                          width: 2,
-                        ),
-                      )
-                    : null,
-              ),
-              child: const Icon(Icons.home_outlined),
-            ),
-            label: 'Home',
+          Icon(
+            Icons.home_outlined,
+            size: 30,
+            color: currentIndex == 0 ? primaryColor : greyShade2,
           ),
-          BottomNavigationBarItem(
-            icon: Container(
-              padding: const EdgeInsets.only(top: 10),
-              decoration: BoxDecoration(
-                border: currentIndex == 1
-                    ?  Border(
-                  top: BorderSide(
-                    color: hoverColor,
-                    width: 2,
-                  ),
-                )
-                    : null,
-              ),
-              child: const Icon(Icons.history),
-            ),
-            label: 'History',
+          Icon(
+            Icons.history,
+            size: 30,
+            color: currentIndex == 1 ? primaryColor : greyShade2,
           ),
-          BottomNavigationBarItem(
-            icon: Container(
-              padding: const EdgeInsets.only(top: 10),
-              decoration: BoxDecoration(
-                border: currentIndex == 2
-                    ?  Border(
-                  top: BorderSide(
-                    color: hoverColor,
-                    width: 2,
-                  ),
-                )
-                    : null,
-              ),
-              child: const Icon(Icons.event_note_outlined),
-            ),
-            label: 'Subscriptions',
+          Icon(
+            Icons.event_note_outlined,
+            size: 30,
+            color: currentIndex == 2 ? primaryColor : greyShade2,
           ),
-          BottomNavigationBarItem(
-            icon: Container(
-              padding: const EdgeInsets.only(top: 10),
-              decoration: BoxDecoration(
-                border: currentIndex == 3
-                    ?  Border(
-                  top: BorderSide(
-                    color: hoverColor,
-                    width: 2,
-                  ),
-                )
-                    : null,
-              ),
-              child: const Icon(Icons.account_balance_wallet_rounded),
-            ),
-            label: 'Wallet',
+          Icon(
+            Icons.account_balance_wallet_outlined,
+            size: 30,
+            color: currentIndex == 3 ? primaryColor : greyShade2,
           ),
-          BottomNavigationBarItem(
-            icon: Container(
-              padding: const EdgeInsets.only(top: 10),
-              decoration: BoxDecoration(
-                border: currentIndex == 4
-                    ?  Border(
-                  top: BorderSide(
-                    color: hoverColor,
-                    width: 2,
-                  ),
-                )
-                    : null,
-              ),
-              child: const Icon(Icons.account_circle_outlined),
-            ),
-            label: 'Account',
+          Icon(
+            Icons.account_circle_outlined,
+            size: 30,
+            color: currentIndex == 4 ? primaryColor : greyShade2,
           ),
         ],
+        color: Colors.white,
+        buttonBackgroundColor: Colors.white,
+        backgroundColor: primaryColor,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: const Duration(milliseconds: 600),
+        onTap: changeScreen,
+        letIndexChange: (index) => true,
       ),
       body: _screens[currentIndex],
     );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //UNCOMMENT AND COMMENT UP -- TRADITIONAL BOTTOM NAV AND THIS CONCEPT
+    // FOLLOWS EXACTLY THE DESIGN OF THE PROTOTYPE AM USING BUT I LOOK COOL THINGS AND I CAN'T HELP :)
+
+
+    // return Scaffold(
+    //   bottomNavigationBar: BottomNavigationBar(
+    //     currentIndex: currentIndex,
+    //     onTap: changeScreen,
+    //     selectedItemColor: hoverColor,
+    //     unselectedItemColor: greyShade2,
+    //     showUnselectedLabels: true,
+    //     elevation: 0,
+    //     items: [
+    //       BottomNavigationBarItem(
+    //         icon: Container(
+    //           padding: const EdgeInsets.only(top: 10),
+    //           decoration: BoxDecoration(
+    //             border: currentIndex == 0
+    //                 ?  Border(
+    //                     top: BorderSide(
+    //                       color: hoverColor,
+    //                       width: 2,
+    //                     ),
+    //                   )
+    //                 : null,
+    //           ),
+    //           child: const Icon(Icons.home_outlined),
+    //         ),
+    //         label: 'Home',
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: Container(
+    //           padding: const EdgeInsets.only(top: 10),
+    //           decoration: BoxDecoration(
+    //             border: currentIndex == 1
+    //                 ?  Border(
+    //               top: BorderSide(
+    //                 color: hoverColor,
+    //                 width: 2,
+    //               ),
+    //             )
+    //                 : null,
+    //           ),
+    //           child: const Icon(Icons.history),
+    //         ),
+    //         label: 'History',
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: Container(
+    //           padding: const EdgeInsets.only(top: 10),
+    //           decoration: BoxDecoration(
+    //             border: currentIndex == 2
+    //                 ?  Border(
+    //               top: BorderSide(
+    //                 color: hoverColor,
+    //                 width: 2,
+    //               ),
+    //             )
+    //                 : null,
+    //           ),
+    //           child: const Icon(Icons.event_note_outlined),
+    //         ),
+    //         label: 'Subscriptions',
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: Container(
+    //           padding: const EdgeInsets.only(top: 10),
+    //           decoration: BoxDecoration(
+    //             border: currentIndex == 3
+    //                 ?  Border(
+    //               top: BorderSide(
+    //                 color: hoverColor,
+    //                 width: 2,
+    //               ),
+    //             )
+    //                 : null,
+    //           ),
+    //           child: const Icon(Icons.account_balance_wallet_rounded),
+    //         ),
+    //         label: 'Wallet',
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: Container(
+    //           padding: const EdgeInsets.only(top: 10),
+    //           decoration: BoxDecoration(
+    //             border: currentIndex == 4
+    //                 ?  Border(
+    //               top: BorderSide(
+    //                 color: hoverColor,
+    //                 width: 2,
+    //               ),
+    //             )
+    //                 : null,
+    //           ),
+    //           child: const Icon(Icons.account_circle_outlined),
+    //         ),
+    //         label: 'Account',
+    //       ),
+    //     ],
+    //   ),
+    //   body: _screens[currentIndex],
+    // );
   }
 }
