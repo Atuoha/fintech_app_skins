c44df094ad9765d45be06c00ce6aa972e1f664cd
Atuoha <atuohainitiatives@gmail.com>
2022-08-16 15:24:37 +0100

    home linear gradient

--- lib/screens/main/home.dart
+++ lib/screens/main/home.dart
@@ -1,3 +1,4 @@
+import 'package:fintech_app_ui/constants/color.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,8 +12,21 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('HOME'),
+      constraints: const BoxConstraints.expand(),
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: FractionalOffset(0.6, 0.8),
+          end: FractionalOffset(0.9, 0.0),
+          stops: [0.4, 0.4],
+          tileMode: TileMode.clamp,
+          colors: [
+            Colors.white,
+            primaryColor,
+          ],
+        ),
+      ),
+      child: Column(
+        children: [],
       ),
     );
   }
