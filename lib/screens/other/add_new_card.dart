c5105eccb041c16c6ff9bb905308b95ad493ac57
Atuoha <atuohainitiatives@gmail.com>
2022-08-22 16:22:14 +0100

    task: card type and color toggling

--- lib/screens/other/add_new_card.dart
+++ lib/screens/other/add_new_card.dart
@@ -1,12 +1,10 @@
+import 'dart:math';
 import 'package:fintech_app_ui/constants/color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-
 import '../../components/kElevatedButton.dart';
 import '../../components/virtual_card_back.dart';
 import '../../components/virtual_card_front.dart';
-import '../../providers/virtual_card.dart';
 
 class AddNewCard extends StatefulWidget {
   static const routeName = '/newCard';
@@ -17,24 +15,36 @@ class AddNewCard extends StatefulWidget {
   State<AddNewCard> createState() => AddNewCardState();
 }
 
-enum Field { password, pin }
+enum Field { password, pin, cardType, cardColor }
 
 class AddNewCardState extends State<AddNewCard> {
   final _formKey = GlobalKey<FormState>();
   final _passwordController = TextEditingController();
   final _pinController = TextEditingController();
   final _cardTypes = ['Mastercard', 'Visa'];
-  var currentCardType = 'Mastercard';
+  var currentCardType = 'Visa';
+  final _cardColors = ['red', 'green', 'black', 'blue', 'other'];
+  var currentCardColor = 'blue';
   var string = "Please be sure your BVN \nis linked with your account";
 
   // this shows whether the card is showing the front or the back
   var cardFront = true;
   var passwordObscure = true;
   var pinObscure = true;
+  int cvc = Random().nextInt(888);
+
+  // var cardNumber // this should be generated
+  var bvnLinkingVisible = true;
 
   toggleCardFace() {
     setState(() {
       cardFront = !cardFront;
+    });
+  }
+
+  removeBvnNotice() {
+    setState(() {
+      bvnLinkingVisible = !bvnLinkingVisible;
     });
   }
 
@@ -50,15 +60,144 @@ class AddNewCardState extends State<AddNewCard> {
     super.initState();
   }
 
+  // custom dropdown for card types and card color
+  Widget kDropDownField(
+    String title,
+    String dataValue,
+    List<String> list,
+    Field field,
+  ) {
+    return DropdownButtonFormField(
+      decoration: InputDecoration(
+        label: Text(
+          title,
+          style: const TextStyle(
+            color: primaryColor,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            width: 1,
+            color: primaryColor,
+          ),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            width: 1,
+            color: greyShade2,
+          ),
+        ),
+      ),
+      value: dataValue,
+
+      items: list
+          .map(
+            (data) => DropdownMenuItem(
+              value: data,
+              child: Text(data),
+            ),
+          )
+          .toList(),
+      onChanged: (value) {
+        switch (field) {
+          case Field.cardType:
+            setState(() {
+              currentCardType = value.toString();
+            });
+            break;
+          case Field.cardColor:
+            setState(() {
+              currentCardColor = value.toString();
+            });
+            break;
+        }
+
+      },
+    );
+  }
+
+  // custom textfield for password and pin
+  Widget kTextField(
+    TextEditingController controller,
+    String text,
+    bool obscure,
+    Field field,
+  ) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscure,
+      keyboardType:
+          field == Field.pin ? TextInputType.number : TextInputType.text,
+      textInputAction:
+          field == Field.pin ? TextInputAction.next : TextInputAction.done,
+      validator: (value) {
+        switch (field) {
+          case Field.password:
+            if (value!.length < 8) {
+              return 'Password is not strong';
+            }
+            break;
+
+          case Field.pin:
+            if (value!.length < 3 || value.length > 4) {
+              return 'Pin must be 4 characters';
+            }
+            break;
+        }
+
+        return null;
+      },
+      decoration: InputDecoration(
+        suffixIcon: controller.text.isNotEmpty
+            ? IconButton(
+                onPressed: () => setState(() {
+                  if (field == Field.password) {
+                    passwordObscure = !passwordObscure;
+                  } else {
+                    pinObscure = !pinObscure;
+                  }
+                }),
+                icon: Icon(
+                  obscure ? Icons.visibility : Icons.visibility_off,
+                  color: primaryColor,
+                ),
+              )
+            : const Text(''),
+        label: Text(
+          text,
+          style: const TextStyle(
+            color: primaryColor,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            width: 1,
+            color: primaryColor,
+          ),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            width: 1,
+            color: greyShade2,
+          ),
+        ),
+      ),
+    );
+  }
+
   // submitForm
   submitForm() {
-    var valid = _formKey.currentState!.validate();
-    if(!valid){
-      return null;
-    }
-
+    // var valid = _formKey.currentState!.validate();
+    // if (!valid) {
+    //   return null;
+    // }
 
     // request for new card
+    print(currentCardColor);
   }
 
   @override
@@ -68,77 +207,6 @@ class AddNewCardState extends State<AddNewCard> {
         statusBarColor: Colors.transparent,
       ),
     );
-    var activeCard = Provider.of<VirtualCardData>(context).getActiveCard();
-
-    Widget kTextField(
-      TextEditingController controller,
-      String text,
-      bool obscure,
-      Field field,
-    ) {
-      return TextFormField(
-        controller: controller,
-        obscureText: obscure,
-        keyboardType:
-            field == Field.pin ? TextInputType.number : TextInputType.text,
-        textInputAction:
-            field == Field.pin ? TextInputAction.next : TextInputAction.done,
-        validator: (value) {
-          switch (field) {
-            case Field.password:
-              if (value!.length < 8) {
-                return 'Password is not strong';
-              }
-              break;
-
-            case Field.pin:
-              if (value!.length < 3) {
-                return 'Pin must be 4';
-              }
-              break;
-          }
-
-          return null;
-        },
-        decoration: InputDecoration(
-          suffixIcon: controller.text.isNotEmpty
-              ? IconButton(
-                  onPressed: () => setState(() {
-                    if (field == Field.password) {
-                      passwordObscure = !passwordObscure;
-                    } else {
-                      pinObscure = !pinObscure;
-                    }
-                  }),
-                  icon: Icon(
-                    obscure ? Icons.visibility : Icons.visibility_off,
-                    color: primaryColor,
-                  ),
-                )
-              : const Text(''),
-          label: Text(
-            text,
-            style: const TextStyle(
-              color: primaryColor,
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: const BorderSide(
-              width: 1,
-              color: primaryColor,
-            ),
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: const BorderSide(
-              width: 1,
-              color: greyShade2,
-            ),
-          ),
-        ),
-      );
-    }
 
     return Scaffold(
       appBar: AppBar(
@@ -167,50 +235,67 @@ class AddNewCardState extends State<AddNewCard> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 15),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 12,
-                ),
-                height: 70,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(7),
-                  color: masterYellow,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      string,
-                      textAlign: TextAlign.left,
-                      style: const TextStyle(
-                        color: Colors.white,
+              bvnLinkingVisible
+                  ? Container(
+                      margin: const EdgeInsets.only(bottom: 15),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 5,
                       ),
-                    ),
-                    const Text(
-                      'Link BVN',
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(7),
+                        color: masterYellow,
+                      ),
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: GestureDetector(
+                              onTap: () => removeBvnNotice(),
+                              child: const Icon(
+                                Icons.clear,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                string,
+                                textAlign: TextAlign.left,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              const Text(
+                                'Link BVN',
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
                       ),
                     )
-                  ],
-                ),
-              ),
+                  : const Text(''),
               InkWell(
                 onTap: () => toggleCardFace(), // for switching card face
                 child: Center(
                   child: cardFront
                       ? VirtualCardUI(
-                          cardColor: activeCard.cardColor,
-                          cardName: activeCard.cardName,
-                          cardNumber: activeCard.cardNumber,
-                          expiry: activeCard.expiry,
-                          isMaster: activeCard.isMaster,
+                          cardColor: currentCardColor,
+                          cardName: 'Ujunwa Peace',
+                          cardNumber: '6786 5435 8907 3424',
+                          expiry: '08/26',
+                          isMaster:
+                              currentCardType == 'Mastercard' ? true : false,
                         )
-                      : VirtualCardBack(cvc: activeCard.cvc),
+                      : VirtualCardBack(cvc: cvc),
                 ),
               ),
               const SizedBox(height: 15),
@@ -228,44 +313,11 @@ class AddNewCardState extends State<AddNewCard> {
                         ),
                       ),
                     ),
-                    DropdownButtonFormField(
-                      decoration: InputDecoration(
-                        label: const Text(
-                          'Select card type',
-                          style: TextStyle(
-                            color: primaryColor,
-                          ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: const BorderSide(
-                            width: 1,
-                            color: primaryColor,
-                          ),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: const BorderSide(
-                            width: 1,
-                            color: greyShade2,
-                          ),
-                        ),
-                      ),
-                      value: currentCardType,
-                      items: _cardTypes
-                          .map(
-                            (cardType) => DropdownMenuItem(
-                              value: cardType,
-                              child: Text(cardType),
-                            ),
-                          )
-                          .toList(),
-                      onChanged: (value) {
-                        setState(() {
-                          currentCardType = value.toString();
-                        });
-                      },
-                    ),
+                    kDropDownField('Select card type', currentCardType,
+                        _cardTypes, Field.cardType),
+                    const SizedBox(height: 20),
+                    kDropDownField('Select card color', currentCardColor,
+                        _cardColors, Field.cardColor),
                     const SizedBox(height: 20),
                     kTextField(
                       _passwordController,
