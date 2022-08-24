da79f750546ca4cd574f3c488afbc5f0ebb2defa
Atuoha <atuohainitiatives@gmail.com>
2022-08-24 07:22:29 +0100

    task: bank section-withdrawal

--- lib/screens/other/withdraw.dart
+++ lib/screens/other/withdraw.dart
@@ -13,7 +13,171 @@ class Withdraw extends StatefulWidget {
   State<Withdraw> createState() => _WithdrawState();
 }
 
+enum Field {
+  password,
+  pin,
+  amount,
+}
+
 class _WithdrawState extends State<Withdraw> {
+  final _formKey = GlobalKey<FormState>();
+  final _passwordController = TextEditingController();
+  final _pinController = TextEditingController();
+  final _amountController = TextEditingController();
+  var passwordObscure = true;
+  var pinObscure = true;
+  var currentBank = 0;
+
+  final bankAccounts = [
+    {
+      'name': 'Wema',
+      'image': 'assets/images/wema.png',
+      'number': 0323894530,
+    },
+    {
+      'name': 'GTBank',
+      'image': 'assets/images/gtbank.png',
+      'number': 2023874534,
+    },
+    {
+      'name': 'Access',
+      'image': 'assets/images/access.png',
+      'number': 6532704320,
+    },
+  ];
+
+  Widget kContainer(
+    int index,
+    String name,
+    String imgAsset,
+    int number,
+  ) {
+    return GestureDetector(
+      onTap: () => setState(() {
+        currentBank = index;
+      }),
+      child: Card(
+        elevation: 2,
+        child: Container(
+          width: 100,
+          padding: const EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            border: currentBank == index
+                ? Border.all(
+                    width: 1,
+                    color: primaryColor,
+                  )
+                : null,
+          ),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: Image.asset(imgAsset),
+              ),
+              // const SizedBox(height: 5),
+              Text(
+                name,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  color: currentBank == index ? primaryColor : greyShade2,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                number.toString(),
+                style: TextStyle(
+                  color: currentBank == index ? primaryColor : greyShade2,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // custom textfield for password and pin
+  Widget kTextField(
+    TextEditingController controller,
+    String text,
+    bool obscure,
+    Field field,
+  ) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscure,
+      keyboardType: field == Field.pin
+          ? TextInputType.number
+          : field == Field.amount
+              ? TextInputType.number
+              : TextInputType.text,
+      textInputAction:
+          field == Field.pin ? TextInputAction.done : TextInputAction.next,
+      validator: (value) {
+        switch (field) {
+          case Field.password:
+            if (value!.length < 8) {
+              return 'Password is not strong';
+            }
+            break;
+
+          case Field.pin:
+            if (value!.length < 3 || value.length > 4) {
+              return 'Pin must be 4 characters';
+            }
+            break;
+
+          case Field.amount:
+            if (value!.isEmpty) {
+              return 'Amount can not be empty';
+            }
+        }
+
+        return null;
+      },
+      decoration: InputDecoration(
+        suffixIcon: controller.text.isNotEmpty
+            ? IconButton(
+                onPressed: () => setState(() {
+                  if (field == Field.password) {
+                    passwordObscure = !passwordObscure;
+                  } else {
+                    pinObscure = !pinObscure;
+                  }
+                }),
+                icon: Icon(
+                  obscure ? Icons.visibility : Icons.visibility_off,
+                  color: primaryColor,
+                ),
+              )
+            : const Text(''),
+        label: Text(
+          text,
+          style: const TextStyle(
+            color: primaryColor,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            width: 1,
+            color: primaryColor,
+          ),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            width: 1,
+            color: greyShade2,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
@@ -46,10 +210,61 @@ class _WithdrawState extends State<Withdraw> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 18),
-        child: Column(children: [
-          const BalanceContainer(),
-        ],),
-      )
+        child: Column(
+          children: [
+            const BalanceContainer(),
+            const SizedBox(height: 20),
+            kTextField(
+              _amountController,
+              'Withdrawal Amount',
+              false,
+              Field.amount,
+            ),
+            // 50737557
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Select receiving account',
+                  style: TextStyle(
+                    color: primaryColor,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => {}, // TODO: Navigate to add new bank
+                  child: const Text(
+                    '+ Add new bank',
+                    style: TextStyle(
+                      color: primaryColor,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 105,
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                scrollDirection: Axis.horizontal,
+                itemCount: bankAccounts.length,
+                itemBuilder: (context, index) {
+                  var bank = bankAccounts[index];
+                  return kContainer(
+                    index,
+                    '${bank["name"]}',
+                    '${bank["image"]}',
+                    int.parse(
+                      '${bank['number']}',
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
