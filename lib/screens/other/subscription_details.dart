6a73c95ae25f76716bf7be64ff4223e5a608b98a
Atuoha <atuohainitiatives@gmail.com>
2022-08-26 03:32:05 +0100

    task: subscription adjustment

--- lib/screens/other/subscription_details.dart
+++ lib/screens/other/subscription_details.dart
@@ -27,11 +27,12 @@ class SubscriptionDetails extends StatefulWidget {
 class _SubscriptionDetailsState extends State<SubscriptionDetails> {
   // this shows whether the card is showing the front or the back
   var cardFront = true;
-  var obscure = false;
+  var obscure = true;
   final _pinController = TextEditingController();
   var isLoading = false;
   final double kSize = 100;
   var transId = Random().nextInt(888);
+  final _formKey = GlobalKey<FormState>();
 
   toggleCardFace() {
     setState(() {
@@ -49,7 +50,9 @@ class _SubscriptionDetailsState extends State<SubscriptionDetails> {
   }
 
   _subscribe(String service, var amount) {
-    if (_pinController.text.isNotEmpty) {
+    var valid = _formKey.currentState!.validate();
+
+    if (valid) {
       //  TODO: Implement subscription
 
       setState(() {
@@ -64,8 +67,9 @@ class _SubscriptionDetailsState extends State<SubscriptionDetails> {
 
       var message = '';
       var status = false;
+      var amountParsed = double.parse(amount.toString());
 
-      if (double.parse(amount) > balance) {
+      if (amountParsed > balance) {
         message =
             'Your subscription for $service can not be confirmed. Due to insufficient balance';
         status = false;
@@ -79,7 +83,7 @@ class _SubscriptionDetailsState extends State<SubscriptionDetails> {
           context,
           listen: false,
         ).withdrawFromBalance(
-          double.parse(amount),
+          amountParsed,
         );
       }
 
@@ -90,7 +94,7 @@ class _SubscriptionDetailsState extends State<SubscriptionDetails> {
             builder: (context) => ResponseScreen(
               successStatus: status,
               message: message,
-              amount: double.parse(amount),
+              amount: amountParsed,
               transId: 'swift-$transId',
             ),
           ),
@@ -148,199 +152,205 @@ class _SubscriptionDetailsState extends State<SubscriptionDetails> {
       body: Padding(
         padding: const EdgeInsets.symmetric(
           horizontal: 20.0,
+          vertical: 10,
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset(subscription.imgAsset),
-                Column(
-                  children: [
-                    Text(
-                      subscription.title,
-                      style: const TextStyle(
-                        color: primaryColor,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    const SizedBox(height: 5),
-                    const Text(
-                      'Family Plan',
-                      style: TextStyle(
-                        color: greyShade1,
-                      ),
-                    )
-                  ],
-                ),
-                Column(
-                  children: [
-                    Wrap(
-                      crossAxisAlignment: WrapCrossAlignment.center,
-                      children: const [
-                        Icon(
-                          Icons.calendar_month,
-                          color: masterYellow,
-                        ),
-                        SizedBox(width: 5),
-                        Text(
-                          'Date added',
-                          style: TextStyle(
-                            color: primaryColor,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 5),
-                    Text(
-                      DateFormat('MMMEd').format(subscription.date),
-                      style: const TextStyle(
-                        color: greyShade1,
-                      ),
-                    )
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Amount',
-                  style: TextStyle(
-                    color: primaryColor,
-                  ),
-                ),
-                Column(
-                  children: [
-                    Text(
-                      '\$${subscription.amount}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    const SizedBox(height: 5),
-                    const Text(
-                      'Charge',
-                      style: TextStyle(
-                        color: greyShade1,
-                      ),
-                    )
-                  ],
-                ),
-              ],
-            ),
-            const Text(
-              'Payment method',
-              style: TextStyle(color: primaryColor),
-            ),
-            InkWell(
-              onTap: () => toggleCardFace(), // for switching card face
-              child: Center(
-                child: cardFront
-                    ? VirtualCardUI(
-                        cardColor: activeCard.cardColor,
-                        cardName: activeCard.cardName,
-                        cardNumber: activeCard.cardNumber,
-                        expiry: activeCard.expiry,
-                        isMaster: activeCard.isMaster,
-                      )
-                    : VirtualCardBack(cvc: activeCard.cvc),
-              ),
-            ),
-            const SizedBox(height: 10),
-            TextFormField(
-              controller: _pinController,
-              obscureText: obscure,
-              keyboardType: TextInputType.number,
-              textInputAction: TextInputAction.done,
-              validator: (value) {
-                if (value!.length < 3 || value.length > 4) {
-                  return 'Pin must be 4 characters';
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                suffixIcon: _pinController.text.isNotEmpty
-                    ? IconButton(
-                        onPressed: () => setState(() {
-                          obscure = !obscure;
-                        }),
-                        icon: Icon(
-                          obscure ? Icons.visibility : Icons.visibility_off,
-                          color: primaryColor,
-                        ),
-                      )
-                    : const Text(''),
-                label: const Text(
-                  '4 Digit Pin',
-                  style: TextStyle(
-                    color: primaryColor,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(
-                    width: 1,
-                    color: primaryColor,
-                  ),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(
-                    width: 1,
-                    color: greyShade2,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            isLoading
-                ? Center(
-                    child: LoadingAnimationWidget.fourRotatingDots(
-                      color: primaryColor,
-                      size: kSize,
-                    ),
-                  )
-                : Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(subscription.imgAsset),
+                  Column(
                     children: [
-                      KElevatedButton(
-                        title: 'Subscribe',
-                        icon: Icons.check_circle,
-                        action:
-                            _subscribe(subscription.title, subscription.amount),
+                      Text(
+                        subscription.title,
+                        style: const TextStyle(
+                          color: primaryColor,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
-                      const SizedBox(height: 10),
-                      Center(
-                        child: GestureDetector(
-                          onTap: () => _termsOfUse,
-                          child: RichText(
-                            text: const TextSpan(
-                              text:
-                                  'By clicking "subscribe", you \n agree to our ',
-                              style: TextStyle(
-                                color: greyShade1,
-                              ),
-                              children: [
-                                TextSpan(
-                                  text: 'Terms of Use',
-                                  style: TextStyle(
-                                      color: primaryColor,
-                                      decoration: TextDecoration.underline,
-                                      decorationColor: primaryColor),
-                                ),
-                              ],
-                            ),
-                          ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        'Family Plan',
+                        style: TextStyle(
+                          color: greyShade1,
                         ),
                       )
                     ],
                   ),
-          ],
+                  Column(
+                    children: [
+                      Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: const [
+                          Icon(
+                            Icons.calendar_month,
+                            color: masterYellow,
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            'Date added',
+                            style: TextStyle(
+                              color: primaryColor,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        DateFormat('MMMEd').format(subscription.date),
+                        style: const TextStyle(
+                          color: greyShade1,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Amount',
+                    style: TextStyle(
+                      color: primaryColor,
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'N${subscription.amount}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        'Charge',
+                        style: TextStyle(
+                          color: greyShade1,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              const Text(
+                'Payment method',
+                style: TextStyle(color: primaryColor),
+              ),
+              InkWell(
+                onTap: () => toggleCardFace(), // for switching card face
+                child: Center(
+                  child: cardFront
+                      ? VirtualCardUI(
+                          cardColor: activeCard.cardColor,
+                          cardName: activeCard.cardName,
+                          cardNumber: activeCard.cardNumber,
+                          expiry: activeCard.expiry,
+                          isMaster: activeCard.isMaster,
+                        )
+                      : VirtualCardBack(cvc: activeCard.cvc),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Form(
+                key: _formKey,
+                child: TextFormField(
+                  controller: _pinController,
+                  obscureText: obscure,
+                  keyboardType: TextInputType.number,
+                  textInputAction: TextInputAction.done,
+                  validator: (value) {
+                    if (value!.length < 3 || value.length > 4) {
+                      return 'Pin must be 4 characters';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    suffixIcon: _pinController.text.isNotEmpty
+                        ? IconButton(
+                            onPressed: () => setState(() {
+                              obscure = !obscure;
+                            }),
+                            icon: Icon(
+                              obscure ? Icons.visibility : Icons.visibility_off,
+                              color: primaryColor,
+                            ),
+                          )
+                        : const Text(''),
+                    label: const Text(
+                      '4 Digit Pin',
+                      style: TextStyle(
+                        color: primaryColor,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        width: 1,
+                        color: primaryColor,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        width: 1,
+                        color: greyShade2,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              isLoading
+                  ? Center(
+                      child: LoadingAnimationWidget.fourRotatingDots(
+                        color: primaryColor,
+                        size: kSize,
+                      ),
+                    )
+                  : Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        KElevatedButton(
+                          title: 'Subscribe',
+                          icon: Icons.check_circle,
+                          action:
+                          ()=>_subscribe(subscription.title, subscription.amount),
+                        ),
+                        const SizedBox(height: 10),
+                        Center(
+                          child: GestureDetector(
+                            onTap: () => _termsOfUse,
+                            child: RichText(
+                              text: const TextSpan(
+                                text:
+                                    'By clicking "subscribe", you \n agree to our ',
+                                style: TextStyle(
+                                  color: greyShade1,
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: 'Terms of Use',
+                                    style: TextStyle(
+                                        color: primaryColor,
+                                        decoration: TextDecoration.underline,
+                                        decorationColor: primaryColor),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+            ],
+          ),
         ),
       ),
     );
